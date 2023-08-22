31abf5dc780a7d8115d4d40b1121391029938c29
Mike <sandynomike@gmail.com>
2023-08-22 14:47:13 +0900

    Cleaned up code and comments

--- STM32F030-Delay-lib.c
+++ STM32F030-Delay-lib.c
@@ -1,29 +1,25 @@
+//  ==========================================================================================
 //  STM32F103-Delay-lib.c
-//    Version 1.0   6 Aug 2023    Forked from STM32F103-Delay-lib. Renamed pause() to halt().
-//                                Updated Comments
-//
-//  Target Microcontroller: STM32F103 (Blue Pill) operating at a clock speed of 8 MHz
-//  Mike Shegedin, 6 Aug 2023
-//
+//  ------------------------------------------------------------------------------------------
 //  Code to implement the following routines:
-//  -----------------------------------------
-//  halt(void)
-//    Halts program by entering endless loop.
-//  -----------------------------------------
-//  delay_us( uint32_t d )
-//    Delay d microseconds. Range: 5(?) to 858 million us (approx. 14 min, 18 s)
-//  -----------------------------------------
+//    delay_us( uint32_t d )
+//      Delay d microseconds. Range: 5(?) to 858 million us (approx. 14 min, 18 s)
+//    
+//    halt( void )
+//      Halts program by entering endless loop.
+//  ------------------------------------------------------------------------------------------
+//  https://github.com/EZdenki/STM32F030-Delay-lib
+//  Released under the MIT License
+//  Copyright (c) 2023
+//  Mike Shegedin, EZdenki.com
+//  Version 1.0   6 Aug 2023    Forked from STM32F103-Delay-lib. Renamed pause() to halt().
+//                              Updated Comments
+//  ------------------------------------------------------------------------------------------
+//  Target Device: STM32F030 operating at a clock speed of 8 MHz
+//  ------------------------------------------------------------------------------------------
 
 #ifndef __STM32F103_DELAY_LIB_C
 #define __STM32F103_DELAY_LIB_C
-//  Halt
-//  Halts the program here by entering an endless loop
-//  For debugging.
-void
-halt( void )
-{
-  while(1);
- }
 
 //  delay_us
 //  Input: uint16_t d
@@ -36,5 +32,14 @@ delay_us( uint32_t d)
   for( uint32_t x=0; x< d; x++) ;
 }
 
+
+//  Halt
+//  Halts the program here by entering an endless loop
+//  For debugging.
+void
+halt( void )
+{
+  while(1);
+}
 
 #endif /*__STM32F103_HALT_LIB_C */
